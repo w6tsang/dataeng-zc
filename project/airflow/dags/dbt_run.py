45348dbfa1c5b103910cf31b84dcdf8d8baa7ba5
w6tsang <w.tsang96@gmail.com>
2022-04-04 15:49:04 -0400

    done project

--- project/airflow/dags/dbt_run.py
+++ project/airflow/dags/dbt_run.py
@@ -1,21 +1,23 @@
 from datetime import timedelta
-
+import os
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import datetime
 from airflow.utils.dates import timedelta
 
+path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 
 with DAG(
     'dbt_dag',
     start_date=datetime(2021, 12, 23),
     description='An Airflow DAG to invoke simple dbt commands',
     schedule_interval=timedelta(days=1),
+    depends_on_past= False,
 ) as dag:
 
     dbt_run = BashOperator(
         task_id='dbt_run',
-        bash_command='cd github && dbt run --target prod'
+        bash_command=f'cd {path_to_local_home}/github && dbt run --target prod'
     )
 
     dbt_test = BashOperator(
